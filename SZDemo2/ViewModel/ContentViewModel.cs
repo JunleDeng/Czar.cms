5e4f8ff7bba7791e7e4b6a45cbe65dcf6b840c7c
unknown <961909963@qq.com>
2020-07-15 15:09:13 +0800

    新增实例代码

---
+++ SZDemo2/ViewModel/ContentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SZDemo2.Models;
+
+namespace SZDemo2.ViewModel
+{
+    public class ContentViewModel
+    {
+        /// <summary>
+        /// 内容列表
+        /// </summary>
+        public List<Content> Contents { get; set; }
+    }
+}
